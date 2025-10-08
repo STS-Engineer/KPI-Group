7b31acbf7b7be34dd1cc345622b0c2ac8673953e
STS-Engineer <administration.sts@avocarbon.com>
2025-10-08 09:18:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -620,7 +620,7 @@ const sendKPIEmail = async (responsibleId, week) => {
 // ---------- Schedule weekly email ----------
 let cronRunning = false;
 cron.schedule(
-  "16 09 * * *",
+  "20 09 * * *",
   async () => {
     if (cronRunning) return console.log("⏭️ Cron already running, skip...");
     cronRunning = true;
