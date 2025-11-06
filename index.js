5d78a5a3c40a7f59ad87455548cf04a3544b92a5
STS-Engineer <administration.sts@avocarbon.com>
2025-11-06 14:48:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));   
 
 // ---------- Postgres ----------
 const pool = new Pool({
