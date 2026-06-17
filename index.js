a1e8ed9b0a3c1b6c96998f264d3ff8549428aab8
STS-Engineer <administration.sts@avocarbon.com>
2026-06-17 12:39:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30126,7 +30126,7 @@ let historyValues = historyLabels.map((label) => {
             </div>
 
             <div class="kpi-section">
-              <h3 style="color:#0078D7;margin-bottom:20px;border-bottom:2px solid #0078D7;padding-bottom:8px;">KPI Value</h3>
+              <h3 style="color:#0078D7;margin-bottom:20px;border-bottom:2px solid #0078D7;padding-bottom:8px;">KPI Values</h3>
               <form action="/redirect" method="POST" id="kpiForm" novalidate>
                 <input type="hidden" name="responsible_id" value="${responsible_id}" />
                 <input type="hidden" name="week" value="${week}" />
